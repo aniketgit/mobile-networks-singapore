9fbe62ff9cddbce5059a42071f3f74c9acc4eb79
webonise <aniket.kumar@weboniselab.com>
2019-02-18 13:13:53 +0530

    added splash screen

--- app/src/main/java/singapore/mobiledata/com/mobiledatasingapore/view/activity/SplashActivity.java
+++ app/src/main/java/singapore/mobiledata/com/mobiledatasingapore/view/activity/SplashActivity.java
@@ -1,4 +1,40 @@
 package singapore.mobiledata.com.mobiledatasingapore.view.activity;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import singapore.mobiledata.com.mobiledatasingapore.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer.
+
+           */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
 }
